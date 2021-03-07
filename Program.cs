30686e02ed17442e214e8c2fa3c439ce3eeaf5a6
ugurinanmaz <ugurinanmaz@gmail.com>
2021-03-07 07:37:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Ugur";
+            musteri1.Soyadi = "Inanmaz";
+            musteri1.TcNo = "1234545321";
+            //Gercek Musteri - Tuzel Musteri
+            //SOLID 
+
+            //kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "12345";
+            musteri2.SirketAdi = "Kodlam.io";
+            musteri2.VergiNo = "12345678890";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+
+
+        }
+    }
+}
